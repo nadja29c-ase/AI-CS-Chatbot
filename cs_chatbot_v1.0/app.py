f899fcff7038833faa57c1035dc2591dfd24712b
nadja29c-ase <nadjacarabulea@gmail.com>
2025-09-10 10:49:07 +0200

    Fix: Set debug=False for production deployment

--- cs_chatbot_v1.0/app.py
+++ cs_chatbot_v1.0/app.py
@@ -232,4 +232,4 @@ def get_metrics():
 if __name__ == "__main__":
     startup_validation()  
     port = int(os.environ.get("PORT", 5001))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
